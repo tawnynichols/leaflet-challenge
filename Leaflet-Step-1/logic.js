7b305b0344abf758177401728766909ef56c3270
tawnynichols <65970072+tawnynichols@users.noreply.github.com>
2020-10-19 21:32:04 -0700

    cleaned up code

--- Leaflet-Step-1/logic.js
+++ Leaflet-Step-1/logic.js
@@ -1,6 +1,4 @@
 // Store our API endpoint inside queryUrl
-// var queryUrl = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2014-01-01&endtime=" +
-//   "2014-01-02&maxlongitude=-69.52148437&minlongitude=-123.83789062&maxlatitude=48.74894534&minlatitude=25.16517337";
 
 var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 
